e46e34cae3f8e7d136bf20ec20912dca31f9119a
Razzhivina2000 <72811486+Razzhivina2000@users.noreply.github.com>
2020-12-06 18:20:55 +0300

    Add files via upload

--- 05/serializers.h
+++ 05/serializers.h
@@ -92,7 +92,9 @@ private:
         try {
             uint64_t tmp = std::stoull(text);
             val = tmp;
-        } catch (std::exception) {
+        } catch (const std::invalid_argument&) {
+            return Error::CorruptedArchive;
+        } catch (const std::out_of_range&) {
             return Error::CorruptedArchive;
         }
         return Error::NoError;
